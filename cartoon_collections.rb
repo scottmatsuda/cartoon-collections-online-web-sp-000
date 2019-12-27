e4a402502b2c95c757e7a124fbe71bf6ee1f7982
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-27 17:20:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map do |element|
+    element.upcase + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
